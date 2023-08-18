9bf528ade0a128c8e7ea262978ea858a57cc74b3
Pengkim <pengkim246@gmail.com>
2023-08-18 13:08:57 +1000

    change color

--- assignment1/src/App.js
+++ assignment1/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./Pages/register/Register";
+import Login from "./Pages/Login/Login";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="" element={<Register />} />
+          <Route path="login" element={<Login />} />
           {/* Add other routes here */}
         </Routes>
       </Router>
