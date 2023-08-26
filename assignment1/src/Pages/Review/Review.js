084f2e5cc64269e021e61206a63f1b7a2e93f0d0
tltq1206 <s3878707@rmit.edu.vn>
2023-08-26 20:36:47 +1000

    fix error when guest cannot see the feedback

--- assignment1/src/Pages/Review/Review.js
+++ assignment1/src/Pages/Review/Review.js
@@ -12,7 +12,7 @@ function Review() {
   const user = getUser();
   const navigate = useNavigate();
   const [reviews, setReviews] = useState(getReviewsByTitle(title));
-  const [rating, setRating] = useState(0);  
+  const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
   const [post, setPost] = useState("");
   function handleChange(event) {
@@ -22,8 +22,10 @@ function Review() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    const data = {content: post, rating: rating};
-    createReview(title, data, user.username);
+    const data = { content: post, rating: rating };
+    if (user !== null) {
+      createReview(title, data, user.username);
+    }
     setReviews(getReviewsByTitle(title));
     setPost("");
     setRating(0);
@@ -38,55 +40,76 @@ function Review() {
           <h1>Review</h1>
         </div>
         <div className="review-film">{title}</div>
-        <div className="review-info">
-          {" "}
-          You review as <span>{user.username}</span>:
-        </div>
-        <div className="review-rating">
-          {[...Array(5)].map((star, index) => {
-            index += 1;
-            return (
-              <button
-                type="button"
-                name="rating"
-                value={rating}
-                className={index <= ((rating && hover) || hover) ? "on" : "off"}
-                onClick={() => setRating(index)}
-                onMouseEnter={() => setHover(index)}
-                onMouseLeave={() => setHover(rating)}
-                onDoubleClick={() => {
-                  setRating(0);
-                  setHover(0);
-                }}
-              >
-                <span className="star">&#9733;</span>
-              </button>
-            );
-          })}
-          <p>You rate {rating} stars</p>
-        </div>
-        <div className="review-content">
-          <textarea
-            name="textarea"
-            value={post}
-            className="review-content-textarea"
-            placeholder="Share details of your own experience watching this movie"
-            onChange={handleChange}
-          ></textarea>
-        </div>
-        <div className="review-buttons">
-          <Button
-            style={{ backgroundColor: "grey" }}
-            onClick={() => {
-              if (
-                window.confirm("Are you sure you wish to cancel this form ?")
-              ) {
-                navigate("/");
-              }
-            }}
-            children="Cancel"
-          ></Button>
-          <Button type="Submit" children="Submit" onClick={handleSubmit} />
+        <div>
+          {user !== null ? (
+            <div>
+              <div className="review-info">
+                <p>
+                  You review as <span>{user.username}</span>{" "}
+                </p>
+              </div>
+              <div className="review-rating">
+                {[...Array(5)].map((star, index) => {
+                  index += 1;
+                  return (
+                    <button
+                      type="button"
+                      name="rating"
+                      value={rating}
+                      className={
+                        index <= ((rating && hover) || hover) ? "on" : "off"
+                      }
+                      onClick={() => setRating(index)}
+                      onMouseEnter={() => setHover(index)}
+                      onMouseLeave={() => setHover(rating)}
+                      onDoubleClick={() => {
+                        setRating(0);
+                        setHover(0);
+                      }}
+                    >
+                      <span className="star">&#9733;</span>
+                    </button>
+                  );
+                })}
+                <p>You rate {rating} stars</p>
+              </div>
+              <div className="review-content">
+                <textarea
+                  name="textarea"
+                  value={post}
+                  className="review-content-textarea"
+                  placeholder="Share details of your own experience watching this movie"
+                  onChange={handleChange}
+                ></textarea>
+              </div>
+              <div className="review-buttons">
+                <Button
+                  style={{ backgroundColor: "grey" }}
+                  onClick={() => {
+                    if (
+                      window.confirm(
+                        "Are you sure you wish to cancel this form ?"
+                      )
+                    ) {
+                      navigate("/");
+                    }
+                  }}
+                  children="Cancel"
+                ></Button>
+                <Button
+                  type="Submit"
+                  children="Submit"
+                  onClick={handleSubmit}
+                />
+              </div>
+            </div>
+          ) : (
+            <div className="review-info">
+              <p>
+                You review as <span>Guest</span>
+              </p>
+            </div>
+          )}
         </div>
       </div>
       <div className="reviews">
