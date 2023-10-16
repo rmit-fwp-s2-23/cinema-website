0debd8c87ba1b2ff934ba3a4185ef14cf3fed2dc
tltq1206 <s3878707@rmit.edu.vn>
2023-10-16 19:43:13 +1100

    fix little error

--- assignment1/front-end/src/Pages/Review/Review.js
+++ assignment1/front-end/src/Pages/Review/Review.js
@@ -65,6 +65,7 @@ function Review() {
     // create a review in localStorage
     await createPost(data);
     await updateRating(title);
+    await fetchReviews();
     // create a security check of this account in localStorage
     createSecurity(title, user);
     //add a new review of this film after user submit his/her review
