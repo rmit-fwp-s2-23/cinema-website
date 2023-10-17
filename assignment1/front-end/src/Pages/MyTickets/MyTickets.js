7b0316fcce94bb277436b133fe7d0de2dfacac80
s3913165 <127904482+s3913165@users.noreply.github.com>
2023-10-18 03:11:28 +1100

    fix some color and box

--- assignment1/front-end/src/Pages/MyTickets/MyTickets.js
+++ assignment1/front-end/src/Pages/MyTickets/MyTickets.js
@@ -1,33 +1,40 @@
-
 import React from 'react';
-import { Box, Heading, VStack } from '@chakra-ui/react';
+import { Box, Center, Heading, VStack } from '@chakra-ui/react';
 import { tickets } from './tickets';
+
 
 const MyTickets = () => {
   return (
-    <Box p={4}>
-      <Heading as="h1" size="xl" mb={4}>
-        My Tickets
-      </Heading>
-      <VStack spacing={4} align="start">
-        {tickets.map((ticket) => (
-          <Box
-            key={ticket.id}
-            p={4}
-            borderWidth="1px"
-            borderRadius="md"
-            boxShadow="md"
-            width="100%"
-          >
-            <Heading as="h2" size="lg">
-              {ticket.title}
-            </Heading>
-            <p>name: {ticket.name}</p>
-            <p>Status: {ticket.status}</p>
-          </Box>
-        ))}
-      </VStack>
-    </Box>
+    <Center >
+      <Box p={5} width="40%" margin="0 auto">
+        <Heading as="h1" size="xl" mb={4} align="Center">
+          My Tickets
+        </Heading>
+        <VStack spacing={6} align="start">
+          {tickets.map((ticket) => (
+            
+
+            <Box
+              key={ticket.id}
+              p={4}
+              borderWidth="1px"
+              borderRadius="md"
+              boxShadow="md"
+              width="100%"
+              backgroundColor="#deb992"
+              alignContent="center"
+              >
+              <Heading as="h2" size="lg" color="#051622"> {/* Change text color to black */}
+                {ticket.title}
+              </Heading>
+              <p color="#051622">name: {ticket.name}</p> {/* Change text color to black */}
+              <p color="#051622">Status: {ticket.status}</p> {/* Change text color to black */}
+            </Box>
+            
+          ))}
+        </VStack>
+      </Box>
+    </Center>
   );
 };
 
