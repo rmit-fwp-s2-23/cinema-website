1103f551290268bc94d4b690968c14417f1d7ff9
Pengkim <pengkim246@gmail.com>
2023-10-19 19:23:31 +1100

    finish login page

--- admin/frontend/src/App.js
+++ admin/frontend/src/App.js
@@ -6,9 +6,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
+      <Router>
+
       <Routes>
                   <Route path="/" element={<Login />} />
       </Routes>
+      </Router>
       
     </div>
   );
